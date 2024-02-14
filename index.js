f5114fd9885e1eea090f460938c8e368576525a3
parinovmatvei <160026381+matri4ka@users.noreply.github.com>
2024-02-14 19:54:46 +0700

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('h1');
+
+button.addEventListener('click', function() {
+output.innerHTML = 'Фильмы от а до я';
+});
